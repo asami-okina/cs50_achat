3da0909be0dea7914995ef52fd79520230a842ce
asami <asami.kawasaki.08@gmail.com>
2022-04-23 20:44:43 +0900

    リファクタリング

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -10,6 +10,97 @@ let profileInfo =
 	"searchFlag": true
 }
 
+let friends = [
+	{
+		"direct_chat_room_id": "1",
+		"friend_use_id": "friend 1",
+		"friend_profile_image": require("../../assets/images/friend_profile_image_1.jpg"),
+		"friend_nickname": "asamiasamiasami1"
+	},
+	{
+		"direct_chat_room_id": "2",
+		"friend_use_id": "friend 2",
+		"friend_profile_image": require("../../assets/images/friend_profile_image_2.jpg"),
+		"friend_nickname": "asami2"
+	},
+	{
+		"direct_chat_room_id": "3",
+		"friend_use_id": "friend 3",
+		"friend_profile_image": require("../../assets/images/friend_profile_image_2.jpg"),
+		"friend_nickname": "asami3"
+	},
+	{
+		"direct_chat_room_id": "5",
+		"friend_use_id": "friend 5",
+		"friend_profile_image": require("../../assets/images/friend_profile_image_2.jpg"),
+		"friend_nickname": "asami5"
+	},
+	{
+		"direct_chat_room_id": "6",
+		"friend_use_id": "friend 6",
+		"friend_profile_image": require("../../assets/images/friend_profile_image_2.jpg"),
+		"friend_nickname": "asami6"
+	},
+	{
+		"direct_chat_room_id": "7",
+		"friend_use_id": "friend 7",
+		"friend_profile_image": require("../../assets/images/friend_profile_image_2.jpg"),
+		"friend_nickname": "asami7"
+	},
+	{
+		"direct_chat_room_id": "8",
+		"friend_use_id": "friend 8",
+		"friend_profile_image": require("../../assets/images/friend_profile_image_2.jpg"),
+		"friend_nickname": "asami8"
+	},
+	{
+		"direct_chat_room_id": "9",
+		"friend_use_id": "friend 9",
+		"friend_profile_image": require("../../assets/images/friend_profile_image_2.jpg"),
+		"friend_nickname": "asami9"
+	},
+	{
+		"direct_chat_room_id": "4",
+		"friend_use_id": "friend 4",
+		"friend_profile_image": require("../../assets/images/friend_profile_image_2.jpg"),
+		"friend_nickname": "asami4"
+	},
+	{
+		"direct_chat_room_id": "10",
+		"friend_use_id": "friend 10",
+		"friend_profile_image": require("../../assets/images/friend_profile_image_2.jpg"),
+		"friend_nickname": "asami10"
+	},
+]
+
+let groups = [
+	{
+		"group_chat_room_id": "1",
+		"group_name": "group 1",
+		"group_image": require("../../assets/images/group_image_1.jpg")
+	},
+	{
+		"group_chat_room_id": "2",
+		"group_name": "group 2",
+		"group_image": require("../../assets/images/group_image_2.jpg")
+	},
+	{
+		"group_chat_room_id": "3",
+		"group_name": "group 3",
+		"group_image": require("../../assets/images/group_image_2.jpg")
+	},
+	{
+		"group_chat_room_id": "4",
+		"group_name": "group 4",
+		"group_image": require("../../assets/images/group_image_2.jpg")
+	},
+	{
+		"group_chat_room_id": "5",
+		"group_name": "group 5r",
+		"group_image": require("../../assets/images/group_image_2.jpg")
+	},
+]
+
 export const handlers = [
 	// req: 一致したリクエストに関する情報
 	// res: モックレスポンスを作成するための機能ユーティリティ
@@ -105,28 +196,7 @@ export const handlers = [
 		return res(
 			ctx.status(200),
 			ctx.json(
-				[
-					{
-						"group_chat_room_id": "1",
-						"group_name": "group 1",
-						"group_image": require("../../assets/images/group_image_1.jpg")
-					},
-					{
-						"group_chat_room_id": "2",
-						"group_name": "group 2",
-						"group_image": require("../../assets/images/group_image_2.jpg")
-					},
-					{
-						"group_chat_room_id": "3",
-						"group_name": "group 3",
-						"group_image": require("../../assets/images/group_image_2.jpg")
-					},
-					{
-						"group_chat_room_id": "4",
-						"group_name": "group 4",
-						"group_image": require("../../assets/images/group_image_2.jpg")
-					},
-				]
+				groups
 			),
 		)
 	}),
@@ -155,7 +225,7 @@ export const handlers = [
 		const { userId } = req.params
 		return res(
 			ctx.status(200),
-			ctx.text(4),
+			ctx.text(groups.length),
 		)
 	}),
 	// ユーザの友達数
@@ -163,7 +233,7 @@ export const handlers = [
 		const { userId } = req.params
 		return res(
 			ctx.status(200),
-			ctx.text(4),
+			ctx.text(friends.length),
 		)
 	}),
 	// ユーザーの友達一覧
@@ -172,68 +242,7 @@ export const handlers = [
 		return res(
 			ctx.status(200),
 			ctx.json(
-				[
-					{
-						"direct_chat_room_id": "1",
-						"friend_use_id": "friend 1",
-						"friend_profile_image": require("../../assets/images/friend_profile_image_1.jpg"),
-						"friend_nickname": "asamiasamiasami1"
-					},
-					{
-						"direct_chat_room_id": "2",
-						"friend_use_id": "friend 2",
-						"friend_profile_image": require("../../assets/images/friend_profile_image_2.jpg"),
-						"friend_nickname": "asami2"
-					},
-					{
-						"direct_chat_room_id": "3",
-						"friend_use_id": "friend 3",
-						"friend_profile_image": require("../../assets/images/friend_profile_image_2.jpg"),
-						"friend_nickname": "asami3"
-					},
-					{
-						"direct_chat_room_id": "5",
-						"friend_use_id": "friend 5",
-						"friend_profile_image": require("../../assets/images/friend_profile_image_2.jpg"),
-						"friend_nickname": "asami5"
-					},
-					{
-						"direct_chat_room_id": "6",
-						"friend_use_id": "friend 6",
-						"friend_profile_image": require("../../assets/images/friend_profile_image_2.jpg"),
-						"friend_nickname": "asami6"
-					},
-					{
-						"direct_chat_room_id": "7",
-						"friend_use_id": "friend 7",
-						"friend_profile_image": require("../../assets/images/friend_profile_image_2.jpg"),
-						"friend_nickname": "asami7"
-					},
-					{
-						"direct_chat_room_id": "8",
-						"friend_use_id": "friend 8",
-						"friend_profile_image": require("../../assets/images/friend_profile_image_2.jpg"),
-						"friend_nickname": "asami8"
-					},
-					{
-						"direct_chat_room_id": "9",
-						"friend_use_id": "friend 9",
-						"friend_profile_image": require("../../assets/images/friend_profile_image_2.jpg"),
-						"friend_nickname": "asami9"
-					},
-					{
-						"direct_chat_room_id": "4",
-						"friend_use_id": "friend 4",
-						"friend_profile_image": require("../../assets/images/friend_profile_image_2.jpg"),
-						"friend_nickname": "asami4"
-					},
-					{
-						"direct_chat_room_id": "10",
-						"friend_use_id": "friend 10",
-						"friend_profile_image": require("../../assets/images/friend_profile_image_2.jpg"),
-						"friend_nickname": "asami10"
-					},
-				]
+				friends
 			),
 		)
 	}),
