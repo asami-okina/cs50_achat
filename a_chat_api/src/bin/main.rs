4ebf0a9474f331acebbcf19abedeb51fb1ac79ac
asami <asami.kawasaki.08@gmail.com>
2022-05-31 11:06:36 +0900

    queryとparamsを取得

--- a_chat_api/src/bin/main.rs
+++ a_chat_api/src/bin/main.rs
@@ -14,7 +14,6 @@ use serde::{Serialize, Deserialize};
 use serde_json::{Value, json};
 use std::collections::HashMap;
 
-
 #[tokio::main]
 async fn main() {
     // 外部モジュールの呼び出し(2015年版)
@@ -26,7 +25,7 @@ async fn main() {
         .route("/api/signup/isAvailableUserIdValidation/:user_id", get(is_available_user_id_validation))
         .route("/api/signup", post(sign_up))
         .route("/api/login", post(log_in))
-        .route("/api/users/home", get(search_name));
+        .route("/api/users/:user_id/home", get(search_name));
 
     // localhost:3000 で hyper と共に実行する
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
@@ -34,13 +33,13 @@ async fn main() {
         .await
         .unwrap();
 
-    type PcProfileInfo = HashMap<String, String>;
+    // type PcProfileInfo = HashMap<String, String>;
     
-    let mut pc_profileInfo:PcProfileInfo = HashMap::new();
-    pc_profileInfo.insert(String::from("userId"), String::from("pcAsami"));
-    pc_profileInfo.insert(String::from("nickName"), String::from("あさみん"));
-    pc_profileInfo.insert(String::from("profileImage"), String::from("https://pbs.twimg.com/profile_images/1257586310077796352/XWNIr3Fr_400x400.jpg"));
-    pc_profileInfo.insert(String::from("searchFlag"), String::from("true"));
+    // let mut pc_profileInfo:PcProfileInfo = HashMap::new();
+    // pc_profileInfo.insert(String::from("userId"), String::from("pcAsami"));
+    // pc_profileInfo.insert(String::from("nickName"), String::from("あさみん"));
+    // pc_profileInfo.insert(String::from("profileImage"), String::from("https://pbs.twimg.com/profile_images/1257586310077796352/XWNIr3Fr_400x400.jpg"));
+    // pc_profileInfo.insert(String::from("searchFlag"), String::from("true"));
         
 }
 // 会員登録
@@ -75,7 +74,6 @@ async fn is_available_user_id_validation(Path(params): Path<IsAvailableUserIdVal
     let user_id = params.user_id;
     Json(json!({ "user_id": user_id  }))
 }
-
 // ログイン
 async fn log_in(body_json: Json<Value>) -> Json<Value> {
     // mailの取得
@@ -104,39 +102,42 @@ struct SearchNameParams {
     user_id: String,
 }
 
-// ニックネームまたはグループ名の検索でヒットするユーザーまたはグループ情報の取得
-// queryとPathは両立できないため、どちらか1つしか利用できない(https://docs.rs/axum/0.5.6/axum/extract/index.html#extracting-request-bodies)
-async fn search_name(Query(params): Query<HashMap<String, String>>)-> Json<Value> {
-    // search_textの取得
-    let _search_text = match  params.get("search_text") {
-        Some(search_text) => search_text,
-        None => panic!("error")
-    };
-    // user_idの取得
-    let user_id = match  params.get("user_id") {
-        Some(user_id) => user_id,
-        None => panic!("error")
-    };
-
-    // let mut result = [
-	// 	{
-	// 		"friend": []
-	// 	},
-	// 	{
-	// 		"group": []
-	// 	}
-	// ];
-	// let mut friends;
-	// if (user_id == "pcAsami") {
-	// 	friends = pc_friends
-	// }
-	// if (user_id == "spAsami") {
-	// 	friends = sp_friends
-	// }
-    Json(json!({ "user_id": user_id  }))
+#[derive(Debug, Deserialize, Serialize)]
+struct SearchNameQuery {
+    search_text: String,
 }
 
-// ★次やること
-// ①かひに、search_nameのqueryとPathは同じにして、queryだけ使ってもいいか聞く
-// ②pc_profileInfoのHashmapで、Stringとboolを使うことはできるのか
+// デフォルト値の取得
+impl Default for SearchNameQuery {
+    fn default() -> Self {
+        Self { search_text: String::from("") }
+    }
+}
+
+async fn search_name(
+    Path(params): Path<SearchNameParams>,
+    search_name_query: Option<Query<SearchNameQuery>>,
+) -> Json<Value> {
+    let user_id = params.user_id;
+    let Query(search_name_query) = search_name_query.unwrap_or_default();
+    Json(json!({ "user_id": user_id, "search_text": search_name_query.search_text  }))
+}
+
+
+// ニックネームまたはグループ名の検索でヒットするユーザーまたはグループ情報の取得
+// queryとPathは両立できないため、どちらか1つしか利用できない(https://docs.rs/axum/0.5.6/axum/extract/index.html#extracting-request-bodies)
+// async fn search_name(Query(params): Query<HashMap<String, String>>)-> Json<Value> {
+//     // search_textの取得
+    // let _search_text = match  params.get("search_text") {
+    //     Some(search_text) => search_text,
+    //     None => panic!("error")
+    // };
+//     // user_idの取得
+//     let user_id = match  params.get("user_id") {
+//         Some(user_id) => user_id,
+//         None => panic!("error")
+//     };
+
+//     Json(json!({ "user_id": user_id  }))
+// }
 
