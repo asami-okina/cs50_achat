f56a1f1977b85f9cae69abef2ff7a5aa1d32a094
asami <asami.kawasaki.08@gmail.com>
2022-06-02 20:33:21 +0900

    fetch_friend_listの実装

--- a_chat_api/src/bin/main.rs
+++ a_chat_api/src/bin/main.rs
@@ -35,9 +35,8 @@ async fn main(){
         .route("/api/users/:user_id/groups/leave", post(handler_leave_group))
         .route("/api/users/:user_id/groups/add", post(handler_add_group))
         .route("/api/users/:user_id/group-count", get(handler_fetch_group_count))
-        .route("/api/users/:user_id/friend-count", get(handler_fetch_friend_count));
-
-
+        .route("/api/users/:user_id/friend-count", get(handler_fetch_friend_count))
+        .route("/api/users/:user_id/friends", get(handler_fetch_friend_list));
 
     // localhost:3000 で hyper と共に実行する
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
@@ -703,4 +702,79 @@ async fn fetch_friend_count(pool: &MySqlPool, user_id:&str) -> anyhow::Result<i6
     .unwrap();
 
     Ok(friend_count[0].friend_count)
+}
+
+/*
+  ユーザーの友達一覧取得
+*/
+// handler
+#[derive(Debug, Deserialize, Serialize)]
+struct FetchFriendListPath {
+    user_id: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct FetchFriendListResult {
+    direct_chat_room_id: String,
+    friend_use_id: String,
+    friend_profile_image: Option<String>,
+    friend_nickname: Option<String>
+}
+
+async fn handler_fetch_friend_list(Path(path): Path<FetchFriendListPath>) -> Json<Value> {
+    let user_id = path.user_id;
+
+    let pool = MySqlPool::connect(&env::var("DATABASE_URL").unwrap()).await.unwrap();
+    let friend_list = fetch_friend_list(&pool, &user_id).await.unwrap();
+    Json(json!({ "friend_list": friend_list }))
+}
+
+// SQL実行部分
+async fn fetch_friend_list(pool: &MySqlPool, user_id:&str) -> anyhow::Result<Vec<FetchFriendListResult>>{
+    let friend_list = sqlx::query!(
+        r#"
+            SELECT
+                f.direct_chat_room_id as direct_chat_room_id,
+                u.id as friend_use_id,
+                u.profile_image as friend_profile_image,
+                u.nickname as friend_nickname
+            FROM
+                user as u
+                LEFT JOIN
+                    follow as f
+                ON  u.id = f.to_user_id
+            WHERE
+                u.id IN(
+                    SELECT
+                        f.to_user_id
+                    FROM
+                        follow as f
+                    WHERE
+                        f.from_user_id = ?
+                )
+            "#,
+        user_id
+    )
+    .fetch_all(pool)
+    .await
+    .unwrap();
+
+    let mut result:Vec<FetchFriendListResult> = vec![];
+
+    for row in &friend_list {
+        let friend = FetchFriendListResult {
+            direct_chat_room_id: row.direct_chat_room_id.unwrap().to_string(),
+            friend_use_id: row.friend_use_id.to_string(),
+            friend_profile_image:  match &row.friend_profile_image {
+                Some(friend_profile_image) => Some(friend_profile_image.to_string()),
+                None => None
+            },
+            friend_nickname:  match &row.friend_nickname {
+                Some(friend_nickname) => Some(friend_nickname.to_string()),
+                None => None
+            },
+          };
+          result.push(friend);
+    }
+    Ok(result)
 }
