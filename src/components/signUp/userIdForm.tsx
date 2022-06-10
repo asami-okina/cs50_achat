5c45b6995a4f91f47428e5cbc6219303d52731ec
asami <asami.kawasaki.08@gmail.com>
2022-06-10 18:27:47 +0900

    userIdForm.tsxの型定義

--- src/components/signUp/userIdForm.tsx
+++ src/components/signUp/userIdForm.tsx
@@ -9,6 +9,19 @@ import { UserIdFormDescription } from './_description/userIdFormDescription';
 // constantsSearchStyles
 import { searchStyles } from '../../constants/styles/searchStyles'
 
+type UserIdFormPropsType = {
+	inputAccessoryViewID: string;
+	isCorrectUserIdSymbol: boolean;
+	setIsCorrectUserIdSymbol: React.Dispatch<React.SetStateAction<boolean>>;
+	isCorrectUserIdStringCount: boolean;
+	setIsCorrectUserIdStringCount: React.Dispatch<React.SetStateAction<boolean>>;
+	isAvailableUserId: boolean;
+	setIsAvailableUserId: React.Dispatch<React.SetStateAction<boolean>>;
+	pageType: string;
+	userIdText: string;
+	onChangeUserIdText: React.Dispatch<React.SetStateAction<string>>;
+}
+
 export function UserIdForm({
 	inputAccessoryViewID,
 	isCorrectUserIdSymbol,
@@ -20,14 +33,14 @@ export function UserIdForm({
 	pageType,
 	userIdText,
 	onChangeUserIdText
-}) {
+}: UserIdFormPropsType) {
 
 	// ユーザーIDの説明文表示
-	const [displayUserIdDescription, setDisplayUserIdDescription] = useState(false);
+	const [displayUserIdDescription, setDisplayUserIdDescription] = useState<boolean>(false);
 	// ユーザーIDアイコンのデフォルト表示
-	const [defaultDisplayUserIcons, setDefaultDisplayUserIcons] = useState(false)
+	const [defaultDisplayUserIcons, setDefaultDisplayUserIcons] = useState<boolean>(false)
 	// ユーザーIDの入力フォームの枠線のデフォルト表示
-	const [defaultUserIdBorderColor, setDefaultUserIdBorderColor] = useState(false)
+	const [defaultUserIdBorderColor, setDefaultUserIdBorderColor] = useState<boolean>(false)
 
 	// ユーザーIDフォームのラベル化
 	let textInputUserId;
@@ -40,7 +53,7 @@ export function UserIdForm({
 
 	// ユーザーID(文字数:4文字以上100文字以内)のバリデーション
 	function _userIdStringCountValidation() {
-		let userIdLength = userIdText.length;
+		let userIdLength: number = userIdText.length;
 
 		// ユーザーIDの文字数が4文字以上100文字以内であれば、バリデーションが通る
 		if (userIdLength >= 4 && userIdLength <= 100) {
@@ -54,12 +67,12 @@ export function UserIdForm({
 	async function _isAvailableUserIdValidation() {
 		try {
 			// APIリクエスト
-				const response = await fetch(API_SERVER_URL + `/api/signup/is_available_user_id_validation/${userIdText}`, {
-					method: "GET",
-					headers: {
-						"Content-Type": "application/json"
-					},
-				})
+			const response = await fetch(API_SERVER_URL + `/api/signup/is_available_user_id_validation/${userIdText}`, {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json"
+				},
+			})
 
 			// レスポンスをJSONにする
 			const parse_response = await response.json()
@@ -74,18 +87,14 @@ export function UserIdForm({
 		}
 	}
 
-
-
 	return (
 		<View>
 			<View style={searchStyles.searchBoxStyle}>
 				<View style={searchStyles.searchWrapperStyle}>
 					<Pressable style={searchStyles.searchContainerStyle} onPress={() => textInputUserId.focus()}>
 						<Text style={searchStyles.searchTitleStyle}>UserId</Text>
-						{/* <KeyboardAvoidingView behavior="padding"> */}
 						<View style={defaultUserIdBorderColor ? isCorrectUserIdSymbol && isCorrectUserIdStringCount ? searchStyles.searchViewStyle : [searchStyles.searchViewStyle, searchStyles.inputIncorrectBorderColorStyle] : searchStyles.searchViewStyle}>
 							<Image source={require("../../../assets/images/profile.png")} style={searchStyles.searchIconStyle} />
-							{/* <KeyboardAvoidingView behavior="padding"> */}
 							<TextInput
 								onChangeText={onChangeUserIdText}
 								style={searchStyles.searchContentWithIconStyle}
