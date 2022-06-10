5c36175f5f70b1483f2d4df9038d41747792ebc9
asami <asami.kawasaki.08@gmail.com>
2022-06-10 14:41:02 +0900

    AddGroupMemberの実装

--- a_chat_api/src/bin/main.rs
+++ a_chat_api/src/bin/main.rs
@@ -45,8 +45,8 @@ async fn main(){
         .route("/api/users/:user_id/chat-room", post(handler_update_chat_room_hidden_or_delete))
         .route("/api/users/:user_id/message", get(handler_fetch_message_by_chat_room_id))
         .route("/api/users/:user_id/message", post(handler_post_message))
-        .route("/api/users/:user_id/last-read-time", post(handler_update_last_read_time));
-
+        .route("/api/users/:user_id/last-read-time", post(handler_update_last_read_time))
+        .route("/api/users/:user_id/group-member", post(handler_add_group_member));
 
     // localhost:3000 で hyper と共に実行する
     axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
@@ -2709,6 +2709,60 @@ async fn update_last_read_time(pool: &MySqlPool, user_id: &String,chat_room_type
         .execute(pool)
         .await?
         .rows_affected();
+    }
+
+    Ok(())
+}
+
+/*
+  グループメンバーの追加
+*/
+#[derive(Debug, Deserialize, Serialize)]
+struct AddGroupMemberPath {
+    user_id: String
+}
+#[derive(Debug, Deserialize, Serialize)]
+struct AddGroupMemberJson {
+    group_chat_room_id: u64,
+    add_user_ids: Vec<String>
+}
+
+async fn handler_add_group_member(
+    Path(path): Path<AddGroupMemberPath>,
+    body_json: Json<AddGroupMemberJson>
+) -> Json<Value> {
+    // user_idの取得
+    let _user_id = path.user_id;
+
+    // group_chat_room_idの取得
+    let group_chat_room_id = &body_json.group_chat_room_id;
+
+    // add_user_idsの取得
+    let add_user_ids = &body_json.add_user_ids;
+
+    let pool = MySqlPool::connect(&env::var("DATABASE_URL").unwrap()).await.unwrap();
+    add_group_member(&pool, &group_chat_room_id, add_user_ids).await.unwrap();
+    Json(json!({ "status_code": 200 }))
+}
+
+// SQL実行部分
+async fn add_group_member(pool: &MySqlPool, group_chat_room_id: &u64, add_user_ids: &Vec<String>) -> anyhow::Result<()> {
+    let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
+
+    for user_id in add_user_ids {
+        sqlx::query!(
+            r#"
+                INSERT INTO group_member ( group_chat_room_id, user_id, entry_date, last_read_time )
+                VALUES ( ?, ? , ? , ? )
+            "#,
+            group_chat_room_id,
+            user_id,
+            now,
+            now
+        )
+        .execute(pool)
+        .await
+        .unwrap();
     }
 
     Ok(())
