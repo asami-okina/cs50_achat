eb08cb101ec4829fed9b814024d05f04c3b4188a
backup <backup@ouhanahiroshinoMacBook-Pro-2.local>
2022-07-27 12:25:15 +0900

    target削除

--- websocket.js
+++ websocket.js
@@ -1,2 +1,2 @@
 // 3012ポートの方を記載
-export const sock = new WebSocket("wss://d1ea-61-120-204-212.jp.ngrok.io/websocket");
+export const sock = new WebSocket("wss://7a90-217-178-132-209.jp.ngrok.io/websocket");
